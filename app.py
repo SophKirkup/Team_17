34a9bfa50cc265be577108f3e9c1f82025151d96
Kieran Robson <k.robson1@newcastle.ac.uk>
2022-01-11 13:52:04 +0000

    add - error handling and account page for future use

--- app.py
+++ app.py
@@ -19,6 +19,19 @@ db = SQLAlchemy(app)
 def index():
     return render_template('index.html')
 
+# ERROR PAGE VIEWS
+@app.errorhandler(403)
+def page_forbidden(error):
+     return render_template('403.html'), 403
+
+@app.errorhandler(404)
+def page_not_found(error):
+     return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+     return render_template('500.html'), 500
+
 
 if __name__ == "__main__":
     my_host = "127.0.0.1"
